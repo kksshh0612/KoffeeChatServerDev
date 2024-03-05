811684cfaf1536e3b5e4400d3a4d930cb80f2bb9
Kim-Seong-Ho <kksshh0612@gmail.com>
2024-03-05 21:50:22 +0900

    init

---
+++ src/main/java/teamkiim/koffeechat/KoffeeChatApplication.java
@@ -0,0 +1,13 @@
+package teamkiim.koffeechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoffeeChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoffeeChatApplication.class, args);
+    }
+
+}
