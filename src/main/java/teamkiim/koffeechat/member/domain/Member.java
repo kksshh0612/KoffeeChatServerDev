d9cd1d96fd0d36f85a6b6511e5ddfc5c69b2b202
Kim-Seong-Ho <kksshh0612@gmail.com>
2024-05-06 16:48:12 +0900

    Feat: 회원 도메인 설계

--- src/main/java/teamkiim/koffeechat/member/domain/Member.java
+++ src/main/java/teamkiim/koffeechat/member/domain/Member.java
@@ -7,6 +7,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import teamkiim.koffeechat.post.dev.domain.SkillCategory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -20,14 +24,49 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
     @Enumerated(EnumType.STRING)
     private MemberRole role;
 
-    private String email;
-    private String password;
-    private String nickname;
+//    private List<Long> chatRoomIdList = new ArrayList<>();
+
+    @ElementCollection
+    private List<SkillCategory> interestSkillCategoryList = new ArrayList<>();
+
+    @Builder
+    private Member(String email, String password, String nickname, MemberRole role, List<SkillCategory> interestSkillCategoryList) {
+        this.role = role;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.interestSkillCategoryList = interestSkillCategoryList;
+    }
 
     //== 비지니스 로직 ==//
+
+    /**
+     * Member 생성 매서드
+     * @param role 권한
+     * @param email 이메일
+     * @param password 비밀번호
+     * @param nickname 닉네임
+     * @param interestSkillCategoryList 관심 기술 리스트
+     * @return
+     */
+    public Member create(String email, String password, String nickname, MemberRole role, List<SkillCategory> interestSkillCategoryList){
+        return Member.builder()
+                .email(email)
+                .password(password)
+                .nickname(nickname)
+                .role(role)
+                .interestSkillCategoryList(interestSkillCategoryList)
+                .build();
+    }
 
     /**
      * 비밀번호 암호화
