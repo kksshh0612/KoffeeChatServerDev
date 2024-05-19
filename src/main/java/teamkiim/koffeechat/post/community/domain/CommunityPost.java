e1667d681df5e5ee733b365715c784ec76168be9
Kim-Seong-Ho <kksshh0612@gmail.com>
2024-05-19 23:19:37 +0900

    Feat: 커뮤니티 게시글 도메인 설계

--- src/main/java/teamkiim/koffeechat/post/community/domain/CommunityPost.java
+++ src/main/java/teamkiim/koffeechat/post/community/domain/CommunityPost.java
@@ -1,25 +1,53 @@
 package teamkiim.koffeechat.post.community.domain;
 
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import teamkiim.koffeechat.comment.Comment;
-import teamkiim.koffeechat.post.Post;
+import org.springframework.cglib.core.Local;
+import teamkiim.koffeechat.comment.domain.Comment;
+import teamkiim.koffeechat.member.domain.Member;
+import teamkiim.koffeechat.post.domain.Post;
+import teamkiim.koffeechat.post.domain.PostCategory;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@DiscriminatorValue("Community")
 @Getter
 @NoArgsConstructor
 public class CommunityPost extends Post {
 
-    //게시글 댓글 리스트
-    @OneToMany
-    @JoinColumn(name="comment_id")
-    private List<Comment> commentList = new ArrayList<>();
+    @Builder
+    public CommunityPost(Member member, PostCategory postCategory, String title, String bodyContent,
+                         Long viewCount, Long likeCount, LocalDateTime createdTime, LocalDateTime modifiedTime) {
+
+        super(member, postCategory, title, bodyContent, viewCount, likeCount, createdTime, modifiedTime);
+    }
+
+    //== 비지니스 로직 ==//
+
+    /**
+     * CommunityPost 완성
+     * @param title 제목
+     * @param bodyContent 본문
+     * @param createdTime 작성 시간
+     */
+    public void completeCommunityPost(String title, String bodyContent, LocalDateTime createdTime){
+
+        complete(PostCategory.COMMUNITY, title, bodyContent, createdTime);
+    }
+
+    /**
+     * CommunityPost 수정
+     * @param title 제목
+     * @param bodyContent 본문
+     * @param modifiedTime 수정 시간
+     */
+    public void modify(String title, String bodyContent, LocalDateTime modifiedTime){
+
+        modify(title, bodyContent, modifiedTime);
+    }
+
 }
