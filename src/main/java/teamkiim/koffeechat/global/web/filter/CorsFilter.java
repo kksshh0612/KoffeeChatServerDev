eedef7e669331e4000e95cfe4bc21d8e1cdd236c
Kim-Seong-Ho <kksshh0612@gmail.com>
2024-06-06 22:29:00 +0900

    Feat : CORS 문제 해결

--- src/main/java/teamkiim/koffeechat/global/web/filter/CorsFilter.java
+++ src/main/java/teamkiim/koffeechat/global/web/filter/CorsFilter.java
@@ -14,18 +14,8 @@ import java.io.IOException;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter extends OncePerRequestFilter {
 
-//    @Override
-//    public void init(FilterConfig filterConfig) throws ServletException {
-//        Filter.super.init(filterConfig);
-//    }
-
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("필터 시작");
-
-//        HttpServletRequest req = (HttpServletRequest) request;
-//        HttpServletResponse res = (HttpServletResponse) response;
 
         response.setHeader("Access-Control-Allow-Origin", "http://**");
         response.setHeader("Access-Control-Allow-Credentials", "true");
@@ -40,31 +30,4 @@ public class CorsFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
         }
     }
-
-//    @Override
-//    public void doFilterInternal(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-//
-//        System.out.println("필터 시작");
-//
-//        HttpServletRequest req = (HttpServletRequest) request;
-//        HttpServletResponse res = (HttpServletResponse) response;
-//
-//        res.setHeader("Access-Control-Allow-Origin", "http://**");
-//        res.setHeader("Access-Control-Allow-Credentials", "true");
-//        res.setHeader("Access-Control-Allow-Methods","*");
-//        res.setHeader("Access-Control-Max-Age", "3600");
-//        res.setHeader("Access-Control-Allow-Headers",
-//                "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//
-//        if("OPTIONS".equalsIgnoreCase(req.getMethod())) {
-//            res.setStatus(HttpServletResponse.SC_OK);
-//        }else {
-//            chain.doFilter(req, res);
-//        }
-//    }
-//
-//    @Override
-//    public void destroy() {
-//        Filter.super.destroy();
-//    }
 }
