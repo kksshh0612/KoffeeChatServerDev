d3e17f7f60ec6c84bbb3856f92e3e8e3a6b6df7c
Kim-Seong-Ho <kksshh0612@gmail.com>
2024-06-06 22:27:32 +0900

    Fix : 프로필 수정, 댓글 수정 dto 수정

--- src/main/java/teamkiim/koffeechat/comment/dto/request/ModifyCommentServiceRequest.java
+++ src/main/java/teamkiim/koffeechat/comment/dto/request/ModifyCommentServiceRequest.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import teamkiim.koffeechat.comment.domain.Comment;
-import teamkiim.koffeechat.member.domain.Member;
-import teamkiim.koffeechat.post.domain.Post;
 
 import java.time.LocalDateTime;
 
