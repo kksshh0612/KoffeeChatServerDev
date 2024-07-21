980653f67c17662cb9b1e48d65b3359abe502ce3
Kim-Seong-Ho <kksshh0612@gmail.com>
2024-07-21 20:08:09 +0900

    Feat : TestSupport 추가

---
+++ src/test/java/teamkiim/koffeechat/TestSupport.java
@@ -0,0 +1,9 @@
+package teamkiim.koffeechat;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public abstract class TestSupport {
+}
