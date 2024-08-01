5f005fc184d849f668b7618e2c8fe128f25527be
Kim-Seong-Ho <kksshh0612@gmail.com>
2024-08-01 13:43:16 +0900

    Feature : 관심기술 fetch join 추가

--- src/main/java/teamkiim/koffeechat/domain/member/repository/MemberRepository.java
+++ src/main/java/teamkiim/koffeechat/domain/member/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package teamkiim.koffeechat.domain.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import teamkiim.koffeechat.domain.member.domain.Member;
 
 import java.util.Optional;
@@ -8,6 +10,9 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findById(Long id);
+
+    @Query("select m from Member m left join fetch m.interestSkillCategoryList where m.id = :id")
+    Optional<Member> findByIdWithSkillCategories(@Param("id") Long id);
 
     Optional<Member> findByEmail(String email);
 }
