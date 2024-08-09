820e1b8e9464b1babe914b315fe9921fa0dbbd56
alswnsp411 <alswnsp411@naver.com>
2024-08-09 19:23:54 +0900

    Fix : 회원 닉네임 형식 지정 삭제

--- src/main/java/teamkiim/koffeechat/domain/member/controller/dto/ModifyProfileRequest.java
+++ src/main/java/teamkiim/koffeechat/domain/member/controller/dto/ModifyProfileRequest.java
@@ -3,12 +3,11 @@ package teamkiim.koffeechat.domain.member.controller.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import teamkiim.koffeechat.domain.member.dto.request.ModifyProfileServiceRequest;
 import teamkiim.koffeechat.domain.member.domain.MemberRole;
+import teamkiim.koffeechat.domain.member.dto.request.ModifyProfileServiceRequest;
 
 @Getter
 @NoArgsConstructor
@@ -17,7 +16,6 @@ import teamkiim.koffeechat.domain.member.domain.MemberRole;
 public class ModifyProfileRequest {
 
     @Schema(description = "회원 닉네임 수정", example ="커피챗수정" )
-    @Pattern(regexp = "^[a-zA-Z0-9가-힣_.-]{3,10}$", message = "닉네임은 3~10자의 알파벳 대소문자, 숫자, _ . -만 사용 가능합니다.")
     @NotBlank(message = "닉네임 입력은 필수입니다")
     private String nickname;
 
