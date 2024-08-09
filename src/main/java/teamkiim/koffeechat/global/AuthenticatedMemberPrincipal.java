2b6c3b073da34599b869c2061f9442a881ab415f
alswnsp411 <alswnsp411@naver.com>
2024-08-09 19:26:36 +0900

    Style : 회원 권한 확인 @AuthenticatedMemberPrincipal 회원 권한 어노테이션 축약형으로 변경

--- src/main/java/teamkiim/koffeechat/global/AuthenticatedMemberPrincipal.java
+++ src/main/java/teamkiim/koffeechat/global/AuthenticatedMemberPrincipal.java
@@ -5,13 +5,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import static teamkiim.koffeechat.global.Auth.MemberRole.*;
+
 /**
  * 회원 권한 확인
  * {COMPANY_EMPLOYEE, FREELANCER, STUDENT, COMPANY_EMPLOYEE_TEMP, MANAGER, ADMIN}
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-@Auth(role = {Auth.MemberRole.COMPANY_EMPLOYEE, Auth.MemberRole.FREELANCER, Auth.MemberRole.STUDENT,
-        Auth.MemberRole.COMPANY_EMPLOYEE_TEMP, Auth.MemberRole.MANAGER, Auth.MemberRole.ADMIN})
+@Auth(role = {COMPANY_EMPLOYEE, FREELANCER, STUDENT,
+        COMPANY_EMPLOYEE_TEMP, MANAGER, ADMIN})
 public @interface AuthenticatedMemberPrincipal {
 }
