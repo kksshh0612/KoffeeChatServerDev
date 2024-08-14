9665f004cf3014f5be98f8b5275d7bfe6111d7be
alswnsp411 <alswnsp411@naver.com>
2024-08-14 16:27:41 +0900

    Fix : 파라미터 바인딩 어노테이션 추가

--- src/main/java/teamkiim/koffeechat/domain/bookmark/repository/BookmarkRepository.java
+++ src/main/java/teamkiim/koffeechat/domain/bookmark/repository/BookmarkRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import teamkiim.koffeechat.domain.bookmark.domain.Bookmark;
 import teamkiim.koffeechat.domain.member.domain.Member;
 import teamkiim.koffeechat.domain.post.common.domain.Post;
@@ -15,5 +16,5 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     Optional<Bookmark> findByPostAndMember(Post post, Member member);
 
     @Query("SELECT b FROM Bookmark b WHERE b.member=:member AND TYPE(b.post) =:postType")
-    Page<Bookmark> findAllByMemberAndDType(Member member, Class<? extends Post> postType, PageRequest pageRequest);
+    Page<Bookmark> findAllByMemberAndDType(@Param("member") Member member, @Param("postType") Class<? extends Post> postType, PageRequest pageRequest);
 }
--- src/main/java/teamkiim/koffeechat/domain/comment/repository/CommentRepository.java
+++ src/main/java/teamkiim/koffeechat/domain/comment/repository/CommentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import teamkiim.koffeechat.domain.comment.domain.Comment;
 import teamkiim.koffeechat.domain.member.domain.Member;
 
@@ -14,5 +15,5 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     Optional<Comment> findById(Long id);
 
     @Query("SELECT c FROM Comment c WHERE c.member=:member")
-    Page<Comment> findAllByMember(Member member, PageRequest pageRequest);
+    Page<Comment> findAllByMember(@Param("member") Member member, PageRequest pageRequest);
 }
