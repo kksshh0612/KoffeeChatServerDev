cf3ccbefd4e76d8425641316c924d1a98fb2e8ce
Kim-Seong-Ho <kksshh0612@gmail.com>
2024-08-20 15:47:28 +0900

    Refactor : @Param 추가

--- src/main/java/teamkiim/koffeechat/domain/post/common/repository/PostRepository.java
+++ src/main/java/teamkiim/koffeechat/domain/post/common/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package teamkiim.koffeechat.domain.post.common.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import teamkiim.koffeechat.domain.post.common.domain.Post;
 
 import java.util.Optional;
@@ -8,5 +10,8 @@ import java.util.Optional;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     Optional<Post> findById(Long id);
+
+    @Query("select p from Post p join fetch p.commentList where p.id = :id")
+    Optional<Post> findByIdWithComments(@Param("id") Long id);
 
 }
--- src/main/java/teamkiim/koffeechat/domain/vote/repository/VoteRecordRepository.java
+++ src/main/java/teamkiim/koffeechat/domain/vote/repository/VoteRecordRepository.java
@@ -2,6 +2,7 @@ package teamkiim.koffeechat.domain.vote.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import teamkiim.koffeechat.domain.member.domain.Member;
 import teamkiim.koffeechat.domain.vote.domain.Vote;
 import teamkiim.koffeechat.domain.vote.domain.VoteRecord;
@@ -11,6 +12,6 @@ import java.util.List;
 public interface VoteRecordRepository extends JpaRepository<VoteRecord, Long> {
 
     @Query("SELECT DISTINCT vr FROM VoteRecord vr JOIN vr.voteItem vi WHERE vi.vote=:vote AND vr.member =:member")
-    List<VoteRecord> findByVoteAndMember(Vote vote, Member member);
+    List<VoteRecord> findByVoteAndMember(@Param("vote") Vote vote, @Param("member") Member member);
 
 }
