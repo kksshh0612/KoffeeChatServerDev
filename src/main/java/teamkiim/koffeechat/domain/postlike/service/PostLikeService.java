c0c10e6747a0b210b98ea5cd3292906f5be72fd1
alswnsp411 <alswnsp411@naver.com>
2024-08-27 16:02:44 +0900

    Fix : 게시글 목록 조회시 DType -> postCategory로 조회 (#56) - 게시글 개발/커뮤니티 유형 별로 목록 조회할때 postCategory로 조회한다. - 삭제된 게시글, 작성 전 게시글에 대해 좋아요, 북마크 할 수 없다. - 이슈 (#56)

--- src/main/java/teamkiim/koffeechat/domain/postlike/service/PostLikeService.java
+++ src/main/java/teamkiim/koffeechat/domain/postlike/service/PostLikeService.java
@@ -3,12 +3,12 @@ package teamkiim.koffeechat.domain.postlike.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import teamkiim.koffeechat.global.exception.CustomException;
-import teamkiim.koffeechat.global.exception.ErrorCode;
 import teamkiim.koffeechat.domain.member.domain.Member;
 import teamkiim.koffeechat.domain.post.common.domain.Post;
 import teamkiim.koffeechat.domain.postlike.domain.PostLike;
 import teamkiim.koffeechat.domain.postlike.repository.PostLikeRepository;
+import teamkiim.koffeechat.global.exception.CustomException;
+import teamkiim.koffeechat.global.exception.ErrorCode;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,20 +17,18 @@ public class PostLikeService {
 
     private final PostLikeRepository postLikeRepository;
 
-    public boolean isMemberLiked(Post post, Member member){
-
-        if(postLikeRepository.findByPostAndMember(post, member).isPresent()) return true;
-
-        return false;
+    public boolean isMemberLiked(Post post, Member member) {
+        return postLikeRepository.findByPostAndMember(post, member).isPresent();
     }
 
     /**
      * 게시물 좋아요
-     * @param post 게시물
+     *
+     * @param post   게시물
      * @param member 좋아요 누른 회원
      */
     @Transactional
-    public void like(Post post, Member member){
+    public void like(Post post, Member member) {
 
         PostLike postLike = PostLike.create(member, post);
 
@@ -39,11 +37,12 @@ public class PostLikeService {
 
     /**
      * 게시물 좋아요 취소
-     * @param post 게시물
+     *
+     * @param post   게시물
      * @param member 좋아요 취소한 회원
      */
     @Transactional
-    public void cancelLike(Post post, Member member){
+    public void cancelLike(Post post, Member member) {
 
         PostLike postLike = postLikeRepository.findByPostAndMember(post, member)
                 .orElseThrow(() -> new CustomException(ErrorCode.LIKE_NOT_FOUND));
