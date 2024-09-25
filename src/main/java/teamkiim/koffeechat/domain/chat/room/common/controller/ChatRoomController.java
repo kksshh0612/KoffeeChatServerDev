0e068c4b150ed6d675596558a1186a11104dd8f9
Seongho Kim <kksshh0612@gmail.com>
2024-09-25 10:45:37 +0900

    Feat : swagger 문서 작성

--- src/main/java/teamkiim/koffeechat/domain/chat/room/common/controller/ChatRoomController.java
+++ src/main/java/teamkiim/koffeechat/domain/chat/room/common/controller/ChatRoomController.java
@@ -3,6 +3,7 @@ package teamkiim.koffeechat.domain.chat.room.common.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.core.Local;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import teamkiim.koffeechat.domain.chat.room.common.domain.ChatRoomType;
@@ -19,7 +20,7 @@ public class ChatRoomController {
     private final ChatRoomService chatRoomService;
 
     /**
-     * 회원이 속한
+     * 회원이 현재 속해있는 채팅방 목록 페이징 조회
      * @param page
      * @param size
      * @param chatRoomType
@@ -48,8 +49,14 @@ public class ChatRoomController {
 ////        return ResponseEntity.ok(chatRoomService.findChatRoom(chatRoomId, memberId));
 //    }
 
+    /**
+     * 채팅방 종료 (채팅창을 닫을 때 호출하는 API)
+     * @param chatRoomId
+     * @param request
+     * @return
+     */
     @AuthenticatedMemberPrincipal
-    @GetMapping("/close/{chatRoomId}")
+    @PatchMapping("/close/{chatRoomId}")
     public ResponseEntity<?> closeChatRoom(@PathVariable("chatRoomId") Long chatRoomId, HttpServletRequest request) {
 
         Long memberId = Long.valueOf(String.valueOf(request.getAttribute("authenticatedMemberPK")));
@@ -57,6 +64,25 @@ public class ChatRoomController {
         LocalDateTime closeTime = LocalDateTime.now();
 
         chatRoomService.close(chatRoomId, memberId, closeTime);
+
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * 채팅방 퇴장 (채팅방에서 완전히 퇴장할 때 호출하는 API)
+     * @param chatRoomId
+     * @param request
+     * @return
+     */
+    @AuthenticatedMemberPrincipal
+    @PatchMapping("/exit/{chatRoomId}")
+    public ResponseEntity<?> exitChatRoom(@PathVariable("chatRoomId") Long chatRoomId, HttpServletRequest request) {
+
+        Long memberId = Long.valueOf(String.valueOf(request.getAttribute("authenticatedMemberPK")));
+
+        LocalDateTime exitTime = LocalDateTime.now();
+
+        chatRoomService.exit(chatRoomId, memberId, exitTime);
 
         return ResponseEntity.ok().build();
     }
