c680f27d62daf67a3051415b31e4d7dd3980c808
Seongho Kim <kksshh0612@gmail.com>
2024-10-29 21:50:13 +0900

    Refactor : WebConfig 클래스 환경에 따라 분리

---
+++ src/main/java/teamkiim/koffeechat/global/web/config/ProdWebConfig.java
@@ -0,0 +1,56 @@
+package teamkiim.koffeechat.global.web.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import teamkiim.koffeechat.global.authentication.Authenticator;
+import teamkiim.koffeechat.global.web.interceptor.AuthInterceptor;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebMvc
+@Profile("prod")
+public class ProdWebConfig implements WebMvcConfigurer {
+
+    private final Authenticator authenticator;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver(){
+        return new StandardServletMultipartResolver();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://**", "https://**", "ws://**")
+                .allowCredentials(true)
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*");
+
+        WebMvcConfigurer.super.addCorsMappings(registry);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new AuthInterceptor(authenticator));
+    }
+}
