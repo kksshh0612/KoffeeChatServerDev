48d6709f609d6f0cd6f1f89a26bc065ae48dc251
Seongho Kim <kksshh0612@gmail.com>
2024-10-30 21:54:11 +0900

    Feat : MongoRepository 설정 추가

--- src/main/java/teamkiim/koffeechat/KoffeeChatApplication.java
+++ src/main/java/teamkiim/koffeechat/KoffeeChatApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 //@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAsync
+@EnableMongoRepositories
 public class KoffeeChatApplication {
 
     public static void main(String[] args) {
