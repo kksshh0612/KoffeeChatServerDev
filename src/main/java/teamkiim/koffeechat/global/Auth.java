ff2945e7ca54ed5532288a58fd0a7336f1de2c0b
Seongho Kim <kksshh0612@gmail.com>
2024-11-05 22:38:23 +0900

    Refactor : MemberRole 회원 도메인의 MemberRole로 변경

--- src/main/java/teamkiim/koffeechat/global/Auth.java
+++ src/main/java/teamkiim/koffeechat/global/Auth.java
@@ -4,12 +4,13 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import teamkiim.koffeechat.domain.member.domain.MemberRole;
 
 @Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Auth {
 
-    enum MemberRole {COMPANY_EMPLOYEE, FREELANCER, STUDENT, COMPANY_EMPLOYEE_TEMP, MANAGER, ADMIN};
+//    enum MemberRole {COMPANY_EMPLOYEE, FREELANCER, STUDENT, COMPANY_EMPLOYEE_TEMP, MANAGER, ADMIN};
 
     MemberRole[] role();
 }
