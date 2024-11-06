cfd45e09e7710296ec97947f86528975cff16e74
Seongho Kim <kksshh0612@gmail.com>
2024-11-06 22:58:45 +0900

    Refactor : 회원,채팅방 별 조회 매서드 세분화 분리

--- src/main/java/teamkiim/koffeechat/domain/chat/room/common/repository/MemberChatRoomRepository.java
+++ src/main/java/teamkiim/koffeechat/domain/chat/room/common/repository/MemberChatRoomRepository.java
@@ -1,5 +1,7 @@
 package teamkiim.koffeechat.domain.chat.room.common.repository;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,32 +12,33 @@ import teamkiim.koffeechat.domain.chat.room.common.domain.ChatRoomType;
 import teamkiim.koffeechat.domain.chat.room.common.domain.MemberChatRoom;
 import teamkiim.koffeechat.domain.member.domain.Member;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface MemberChatRoomRepository extends JpaRepository<MemberChatRoom, Long> {
 
-    List<MemberChatRoom> findAllByMember(Member member);
+    @Query("SELECT mcr FROM MemberChatRoom mcr WHERE mcr.member = :member AND mcr.active = true")
+    List<MemberChatRoom> findAllByMember(@Param("member") Member member);
 
-    Optional<MemberChatRoom> findByMemberAndChatRoom(Member member, ChatRoom chatRoom);
+    @Query("SELECT mcr FROM MemberChatRoom mcr WHERE mcr.member = :member AND mcr.chatRoom = :chatRoom AND mcr.active = true")
+    Optional<MemberChatRoom> findByMemberAndActiveChatRoom(@Param("member") Member member,
+                                                           @Param("chatRoom") ChatRoom chatRoom);
+
+    @Query("SELECT mcr FROM MemberChatRoom mcr WHERE mcr.member = :member AND mcr.chatRoom = :chatRoom AND mcr.active = false")
+    Optional<MemberChatRoom> findByMemberAndNotActiveChatRoom(@Param("member") Member member,
+                                                              @Param("chatRoom") ChatRoom chatRoom);
 
     @Query("SELECT mcr FROM MemberChatRoom mcr WHERE mcr.chatRoom = :chatRoom AND mcr.member <> :member")
-    Optional<MemberChatRoom> findByChatRoomExceptMember(@Param("chatRoom") ChatRoom chatRoom, @Param("member") Member member);
+    Optional<MemberChatRoom> findByChatRoomExceptMember(@Param("chatRoom") ChatRoom chatRoom,
+                                                        @Param("member") Member member);
 
-    List<MemberChatRoom> findAllByChatRoom(ChatRoom chatRoom);
-
-//    @Query("SELECT mcr FROM MemberChatRoom mcr WHERE " +
-//            "mcr.member =:member AND " +
-//            "(:chatRoomType IS NULL OR mcr.chatRoom.chatRoomType =:chatRoomType) AND " +
-//            "mcr.chatRoom.id <:cursorId " +
-//            "ORDER BY mcr.chatRoom.lastMessageTime desc")
-//    Page<MemberChatRoom> findAllByMemberAndChatRoomType(@Param("member") Member member, @Param("chatRoomType") ChatRoomType chatRoomType,
-//                                                        @Param("cursorId") int cursorId, Pageable pageable);
+    @Query("SELECT mcr FROM MemberChatRoom mcr WHERE mcr.chatRoom = :chatRoom")
+    List<MemberChatRoom> findAllByChatRoom(@Param("chatRoom") ChatRoom chatRoom);
 
     @Query("SELECT mcr FROM MemberChatRoom mcr WHERE " +
             "mcr.member =:member AND " +
-            "(:chatRoomType IS NULL OR mcr.chatRoom.chatRoomType =:chatRoomType)")
-    Page<MemberChatRoom> findAllByMemberAndChatRoomType(@Param("member") Member member, @Param("chatRoomType") ChatRoomType chatRoomType,
+            "(:chatRoomType IS NULL " +
+            "OR mcr.chatRoom.chatRoomType =:chatRoomType) " +
+            "AND mcr.active = true")
+    Page<MemberChatRoom> findAllByMemberAndChatRoomType(@Param("member") Member member,
+                                                        @Param("chatRoomType") ChatRoomType chatRoomType,
                                                         Pageable pageable);
 
 }
