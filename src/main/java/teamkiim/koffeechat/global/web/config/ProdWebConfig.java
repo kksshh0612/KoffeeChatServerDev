19786983ad693b4ebd06e84505c14756da35c1cb
Seongho Kim <kksshh0612@gmail.com>
2024-11-27 22:16:10 +0900

    fix : cors OriginPattern 세분화

--- src/main/java/teamkiim/koffeechat/global/web/config/ProdWebConfig.java
+++ src/main/java/teamkiim/koffeechat/global/web/config/ProdWebConfig.java
@@ -1,7 +1,6 @@
 package teamkiim.koffeechat.global.web.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,7 +10,10 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import teamkiim.koffeechat.global.authentication.Authenticator;
 import teamkiim.koffeechat.global.web.interceptor.AuthInterceptor;
 
@@ -29,24 +31,31 @@ public class ProdWebConfig implements WebMvcConfigurer {
     }
 
     @Bean
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
     @Bean
-    public MultipartResolver multipartResolver(){
+    public MultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
     }
 
     @Bean
-    ForwardedHeaderFilter forwardedHeaderFilter(){
+    ForwardedHeaderFilter forwardedHeaderFilter() {
         return new ForwardedHeaderFilter();
     }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns("http://**", "https://**", "ws://**")
+//                .allowCredentials(true)
+//                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
+//                .allowedHeaders("*");
+        
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://**", "https://**", "ws://**")
+                .allowedOriginPatterns("http://koffeechat.site/**", "https://koffeechat.site/**",
+                        "ws://koffeechat.site/**")
                 .allowCredentials(true)
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*");
